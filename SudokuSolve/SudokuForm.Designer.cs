f152731fd3f9354562c9ab9728bee8698b9a992f
CyMark <CyMark@users.noreply.github.com>
2019-06-26 18:41:36 +0200

    Initial commit

---
+++ SudokuSolve/SudokuForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SudokuSolve
+{
+    partial class SudokuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(35, 564);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 0;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // SudokuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 604);
+            this.Controls.Add(this.buttonQuit);
+            this.Name = "SudokuForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonQuit;
+    }
+}
+
---
+++ SudokuSolve/SudokuForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuSolve
+{
+    public partial class SudokuForm : Form
+    {
+        public SudokuForm()
+        {
+            InitializeComponent();
+            InitApplication();
+        }
+
+        private void InitApplication()
+        {
+
+        }
+
+
+        //--------------------------------------------------------------------------------------------
+        //  UI Events
+        //--------------------------------------------------------------------------------------------
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+
+        }
+    } // class SudokuForm
+}
